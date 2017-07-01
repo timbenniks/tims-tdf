7f4526c8eb9f3d9f2dc25d48d46395732a73bac2
Tim Benniks <tbenniks@gmail.com>
2017-07-01 18:09:46 +0200

    Added front-end code through express middleare

--- index.js
+++ index.js
@@ -3,9 +3,41 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const api = require('./routes/api')
 const config = require('./config.json')
+const browserify = require('browserify-middleware')
+const sass = require('express-sass-middleware')
+const babelify = require('babelify')
+const vueify = require('vueify')
+const autoprefixer = require('autoprefixer')
 
 const app = express()
 const port = process.env.PORT || '5100'
+
+browserify.settings({
+  transform: [
+    [babelify, { presets: ['es2015'], plugins: ['transform-object-assign', 'es6-promise'] }],
+    [vueify]
+  ],
+  production: {
+    cache: true,
+    precompile: true,
+    minify: true,
+    gzip: true,
+    debug: false
+  },
+  development: {
+    cache: 'dynamic',
+    precompile: false,
+    minify: false,
+    gzip: false,
+    debug: true
+  }
+})
+
+vueify.compiler.applyConfig({
+  postcss: [
+    autoprefixer({ browsers: ['last 2 versions', 'ie >= 11', 'Safari >= 9', 'iOS >= 9'] }),
+  ]
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
@@ -23,6 +55,15 @@ app.use((req, res, next) => {
 app.get('/', (req, res) => {
   res.render('index', { config })
 })
+
+app.use('/app.js', browserify(path.join(__dirname, '/public/scripts/app.js')))
+
+app.get('/styles.css', sass({
+  file: path.join(__dirname, '/public/styles/styles.scss'),
+  watch: true,
+  precompile: true,
+  outputStyle: 'compressed'
+}))
 
 app.use('/api/', api)
 app.listen(port)
