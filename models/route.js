a6128a52f6b072d1250c82e751e04e6532107863
Tim Benniks <tbenniks@gmail.com>
2017-07-01 11:58:03 +0200

    Added route with points of interest and lat/lon data

---
+++ models/route.js
@@ -0,0 +1,51 @@
+const getUrl = require('../helpers/getUrl')
+const callApi = require('../helpers/callApi')
+
+module.exports = (state) => new Promise((resolve, reject) => {
+  const getPointOfInterestType = point => {
+    let type = ''
+    switch (point) {
+      case 1: type = 'SPRINT'; break
+      case 2: type = 'CLIMB'; break
+      case 3: type = 'FINISH'; break
+      case 4: type = 'TRIAL'; break
+      case 7: type = 'START'; break
+      default: type = ''; break
+    }
+
+    return type
+  }
+
+  const cleanPointsOfInterest = points => points.map(point => ({
+    lat: point.Latitude,
+    lon: point.Longitude,
+    type: getPointOfInterestType(point.PointOfInterestType),
+    name: point.CheckpointName,
+    distFromStart: point.DistanceFromStart,
+    distToFinish: point.DistanceToFinish,
+    climbCat: point.ClimbCategory,
+    sequence: point.Sequence,
+    altitude: point.Altitude || false,
+    angle: point.Angle || false,
+    length: point.Length || false,
+    keyword: point.POIKeyword || false,
+    image: point.PoiImage || false,
+    results: point.Results && point.Results.length || false
+  }))
+
+  const cleanPoints = points => points.map(point => ({
+    lat: point.Latitude,
+    lon: point.Longitude,
+    distFromStart: point.DistanceFromStart,
+    altitude: point.Altitude
+  }))
+
+  callApi(getUrl('route', state.stage))
+    .then(response => {
+      resolve({
+        pointsOfInterest: cleanPointsOfInterest(response.PointsOfInterest),
+        points: cleanPoints(response.profilePoints)
+      })
+    })
+    .catch(reject)
+})
